69853d75c6f071d21edc1cb2170c85135cb5881b
Alexander Sokolovsky <amsokol@gmail.com>
2015-10-08 10:43:30 +0600

    #5

---
+++ kendoui/request.go
@@ -0,0 +1,39 @@
+package kendoui
+
+import (
+	"net/url"
+	"strings"
+)
+
+// filter[filters][0][field]=Title
+// filter[filters][0][ignoreCase]=true
+// filter[filters][0][operator]=startswith
+// filter[filters][0][value]=the
+// filter[logic]=and
+
+type RequestParams map[string]interface{}
+
+func ParseParams(vs url.Values) RequestParams {
+	params := make(RequestParams)
+
+	for key, value := range vs {
+		p := params
+
+		keys := strings.Split(key, "[")
+		len := len(keys)
+		key = keys[0]
+
+		for i := 1; i < len; i++ {
+			p1, ok := p[key]
+			if !ok {
+				p1 = make(RequestParams)
+				p[key] = p1
+			}
+			p = (p1).(RequestParams)
+			key = strings.TrimRight(keys[i], "]")
+		}
+		p[key] = value
+	}
+
+	return params
+}
